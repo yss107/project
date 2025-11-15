d4cc851b649267406a3c39c417d544738d095997
Yash kumar <111907417+yss107@users.noreply.github.com>
2025-11-15 16:05:46 +0530

    Add files via upload

---
+++ ds-assignment-master/src/features/transformations.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from haversine import haversine
+
+from src.utils.time import robust_hour_of_iso_date
+
+
+def driver_distance_to_pickup(df: pd.DataFrame) -> pd.DataFrame:
+    df["driver_distance"] = df.apply(
+        lambda r: haversine(
+            (r["driver_latitude"], r["driver_longitude"]),
+            (r["pickup_latitude"], r["pickup_longitude"]),
+        ),
+        axis=1,
+    )
+    return df
+
+
+def hour_of_day(df: pd.DataFrame) -> pd.DataFrame:
+    df["event_hour"] = df["event_timestamp"].apply(robust_hour_of_iso_date)
+    return df
+
+
+def driver_historical_completed_bookings(df: pd.DataFrame) -> pd.DataFrame:
+    raise NotImplementedError(
+        f"Show us your feature engineering skills! Suppose that drivers with a good track record are more likely to accept bookings. "
+        f"Implement a feature that describes the number of historical bookings that each driver has completed."
+    )
---
+++ ds-assignment-master/src/models/classifier.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator
+
+
+class Classifier(ABC):
+    @abstractmethod
+    def train(self, *params) -> None:
+        pass
+
+    @abstractmethod
+    def evaluate(self, *params) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def predict(self, *params) -> np.ndarray:
+        pass
+
+
+class SklearnClassifier(Classifier):
+    def __init__(
+        self, estimator: BaseEstimator, features: List[str], target: str,
+    ):
+        self.clf = estimator
+        self.features = features
+        self.target = target
+
+    def train(self, df_train: pd.DataFrame):
+        self.clf.fit(df_train[self.features].values, df_train[self.target].values)
+
+    def evaluate(self, df_test: pd.DataFrame):
+        raise NotImplementedError(
+            f"You're almost there! Identify an appropriate evaluation metric for your model and implement it here. "
+            f"The expected output is a dictionary of the following schema: {{metric_name: metric_score}}"
+        )
+
+    def predict(self, df: pd.DataFrame):
+        return self.clf.predict_proba(df[self.features].values)[:, 1]
